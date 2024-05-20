b5411f2e6864d07009b63bd7f32fc4e9e1b7ed99
ducanhaovai <ducanhaovai@users.noreply.github.com>
2024-05-20 18:39:26 +0700

    update

--- js/script.js
+++ js/script.js
@@ -56,3 +56,42 @@ document.addEventListener("DOMContentLoaded", function () {
     clearTimeout(timer2);
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const submitBtn = document.getElementById("submitcheck");
+  const toast = document.querySelector(".alert-t");
+  const progress = document.querySelector(".progress");
+
+  let timer1, timer2;
+
+  function showToast() {
+    toast.classList.add("active");
+
+    progress.classList.add("active");
+
+    timer1 = setTimeout(() => {
+      toast.classList.remove("active");
+    }, 5000);
+
+    timer2 = setTimeout(() => {
+      progress.classList.remove("active");
+    }, 5300);
+  }
+
+  submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    showToast();
+  });
+
+  const closeIcon = document.querySelector(".close");
+  closeIcon.addEventListener("click", () => {
+    toast.classList.remove("active");
+
+    setTimeout(() => {
+      progress.classList.remove("active");
+    }, 300);
+
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+  });
+});
